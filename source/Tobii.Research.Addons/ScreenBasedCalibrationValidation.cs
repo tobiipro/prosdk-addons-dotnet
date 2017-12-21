0216e667b3b00f7cdaf3d8a7a6f43ab651d077e3
Anders Clausen <anders.clausen@tobii.com>
2017-12-21 09:10:31 +0100

    Adding implementation of screen based calibration validation.

--- source/Tobii.Research.Addons/ScreenBasedCalibrationValidation.cs
+++ source/Tobii.Research.Addons/ScreenBasedCalibrationValidation.cs
@@ -1,6 +1,293 @@
-﻿namespace Tobii.Research.Addons
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Tobii.Research.Addons.Utility;
+
+namespace Tobii.Research.Addons
 {
+    public sealed class CalibrationValidationResult
+    {
+        public List<CalibrationValidationPoint> Points { get; internal set; }
+
+        public float AverageAccuracy { get; internal set; }
+
+        public float AveragePrecision { get; internal set; }
+
+        public CalibrationValidationResult()
+        {
+            Points = new List<CalibrationValidationPoint>();
+        }
+
+        internal void UpdateResult(List<CalibrationValidationPoint> points, float averageAccuracy, float averagePrecision)
+        {
+            Points = points;
+            AverageAccuracy = averageAccuracy;
+            AveragePrecision = averagePrecision;
+        }
+    }
+
+    public sealed class CalibrationValidationPoint
+    {
+        public NormalizedPoint2D Coordinates { get; private set; }
+
+        public float AccuracyLeftEye { get; private set; }
+
+        public float PrecisionLeftEye { get; private set; }
+
+        public float AccuracyRightEye { get; private set; }
+
+        public float PrecisionRightEye { get; private set; }
+
+        public bool TimedOut { get; private set; }
+
+        public GazeDataEventArgs[] GazeData { get; private set; }
+
+        public CalibrationValidationPoint(NormalizedPoint2D coordinates, float accuracyLeftEye, float precisionLeftEye, float accuracyRightEye, float precisionRightEye, bool timedOut, GazeDataEventArgs[] gazeData)
+        {
+            Coordinates = coordinates;
+            AccuracyLeftEye = accuracyLeftEye;
+            PrecisionLeftEye = precisionLeftEye;
+            AccuracyRightEye = accuracyRightEye;
+            PrecisionRightEye = precisionRightEye;
+            TimedOut = timedOut;
+            GazeData = gazeData;
+        }
+    }
+
     public class ScreenBasedCalibrationValidation
     {
+        public bool IsCollectingData
+        {
+            get
+            {
+                if (_timeKeeper.TimedOut)
+                {
+                    SaveDataForPoint();
+                }
+
+                return State == ValidationState.Collecting;
+            }
+        }
+
+        private enum ValidationState
+        {
+            Idle,
+            NotCollecting,
+            Collecting,
+        }
+
+        private IEyeTracker _eyeTracker;
+        private int _sampleCount;
+        private Queue<GazeDataEventArgs> _data;
+        private List<KeyValuePair<NormalizedPoint2D, Queue<GazeDataEventArgs>>> _dataMap;
+        private TimeKeeper _timeKeeper;
+        private CalibrationValidationResult _latestResult;
+        private NormalizedPoint2D _currentPoint;
+        private readonly object _lock = new object();
+        private ValidationState _state;
+
+        private ValidationState State
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    return _state;
+                }
+            }
+
+            set
+            {
+                lock (_lock)
+                {
+                    _state = value;
+                }
+            }
+        }
+
+        public CalibrationValidationResult Result
+        {
+            get
+            {
+                return _latestResult;
+            }
+        }
+
+        public ScreenBasedCalibrationValidation(IEyeTracker eyeTracker, int sampleCount = 30, int timeoutMS = 1000)
+        {
+            if (eyeTracker == null)
+            {
+                throw new ArgumentException("Eye tracker is null");
+            }
+
+            if (sampleCount < 10 || sampleCount > 3000)
+            {
+                throw new ArgumentException("Samples must be between 10 and 3000");
+            }
+
+            if (timeoutMS < 100 || timeoutMS > 3000)
+            {
+                throw new ArgumentException("Timout must be between 100 and 3000 ms");
+            }
+
+            _eyeTracker = eyeTracker;
+            _sampleCount = sampleCount;
+            _timeKeeper = new TimeKeeper(timeoutMS);
+            State = ValidationState.Idle;
+        }
+
+        public void StartCollectingData(NormalizedPoint2D coords)
+        {
+            if (State == ValidationState.Collecting)
+            {
+                throw new InvalidOperationException("Already in collecting data state");
+            }
+
+            _currentPoint = coords;
+            _timeKeeper.Restart();
+            State = ValidationState.Collecting;
+        }
+
+        public void EnterValidationMode()
+        {
+            if (State != ValidationState.Idle)
+            {
+                throw new InvalidOperationException("Validation mode already entered");
+            }
+
+            _dataMap = new List<KeyValuePair<NormalizedPoint2D, Queue<GazeDataEventArgs>>>();
+            _latestResult = new CalibrationValidationResult();
+            State = ValidationState.NotCollecting;
+            _eyeTracker.GazeDataReceived += OnGazeDataReceived;
+        }
+
+        public void LeaveValidationMode()
+        {
+            if (State == ValidationState.Idle)
+            {
+                throw new InvalidOperationException("Not in validation mode");
+            }
+
+            _eyeTracker.GazeDataReceived -= OnGazeDataReceived;
+            _currentPoint = null;
+            State = ValidationState.Idle;
+        }
+
+        public CalibrationValidationResult Compute()
+        {
+            if (IsCollectingData)
+            {
+                throw new InvalidOperationException("Compute called while collecting data");
+            }
+
+            var points = new List<CalibrationValidationPoint>();
+
+            foreach (var kv in _dataMap)
+            {
+                var targetPoint2D = kv.Key;
+                var samples = kv.Value;
+
+                var targetPoint3D = targetPoint2D.NormalizedPoint2DToPoint3D(_eyeTracker.GetDisplayArea());
+
+                if (samples.Count < _sampleCount)
+                {
+                    // We timed out before collecting enough valid samples.
+                    // Set the timeout flag and continue.
+                    points.Add(new CalibrationValidationPoint(targetPoint2D, -1, -1, -1, -1, true, samples.ToArray()));
+                    continue;
+                }
+
+                var gazePointAverageLeft = samples.Average(s => s.LeftEye.GazePoint.PositionInUserCoordinates);
+                var gazePointAverageRight = samples.Average(s => s.RightEye.GazePoint.PositionInUserCoordinates);
+                var gazeOriginAverageLeft = samples.Average(s => s.LeftEye.GazeOrigin.PositionInUserCoordinates);
+                var gazeOriginAverageRight = samples.Average(s => s.RightEye.GazeOrigin.PositionInUserCoordinates);
+
+                var directionGazePointLeft = gazeOriginAverageLeft.NormalizedDirection(gazePointAverageLeft);
+                var directionTargetLeft = gazeOriginAverageLeft.NormalizedDirection(targetPoint3D);
+                var accuracyLeftEye = directionTargetLeft.Angle(directionGazePointLeft);
+
+                var directionGazePointRight = gazeOriginAverageRight.NormalizedDirection(gazePointAverageRight);
+                var directionTargetRight = gazeOriginAverageRight.NormalizedDirection(targetPoint3D);
+                var accuracyRightEye = directionTargetRight.Angle(directionGazePointRight);
+
+                var varianceLeft = samples.Select(s => Math.Pow(s
+                    .LeftEye.GazeOrigin.PositionInUserCoordinates.NormalizedDirection(s.LeftEye.GazePoint.PositionInUserCoordinates)
+                    .Angle(s.LeftEye.GazeOrigin.PositionInUserCoordinates.NormalizedDirection(gazePointAverageLeft)), 2)).Average();
+
+                var varianceRight = samples.Select(s => Math.Pow(s
+                    .RightEye.GazeOrigin.PositionInUserCoordinates.NormalizedDirection(s.RightEye.GazePoint.PositionInUserCoordinates)
+                    .Angle(s.RightEye.GazeOrigin.PositionInUserCoordinates.NormalizedDirection(gazePointAverageRight)), 2)).Average();
+
+                var precisionLeftEye = varianceLeft > 0 ? Math.Sqrt(varianceLeft) : 0;
+                var precisionRightEye = varianceRight > 0 ? Math.Sqrt(varianceRight) : 0;
+
+                points.Add(new CalibrationValidationPoint(targetPoint2D, (float)accuracyLeftEye, (float)precisionLeftEye, (float)accuracyRightEye, (float)precisionRightEye, false, samples.ToArray()));
+            }
+
+            _latestResult.Points = points;
+
+            var avaragePrecisionLeftEye = _latestResult.Points.Select(p => p.PrecisionLeftEye).Average();
+            var avaragePrecisionRightEye = _latestResult.Points.Select(p => p.PrecisionRightEye).Average();
+            var avarageAccuracyLeftEye = _latestResult.Points.Select(p => p.AccuracyLeftEye).Average();
+            var avarageAccuracyRightEye = _latestResult.Points.Select(p => p.AccuracyRightEye).Average();
+
+            _latestResult.AveragePrecision = (avaragePrecisionLeftEye + avaragePrecisionRightEye) / 2.0f;
+            _latestResult.AverageAccuracy = (avarageAccuracyLeftEye + avarageAccuracyRightEye) / 2.0f;
+
+            return _latestResult;
+        }
+
+        private void OnGazeDataReceived(object sender, GazeDataEventArgs e)
+        {
+            switch (State)
+            {
+                case ValidationState.Idle:
+                    break;
+
+                case ValidationState.NotCollecting:
+                    break;
+
+                case ValidationState.Collecting:
+                    if (_data == null)
+                    {
+                        _data = new Queue<GazeDataEventArgs>();
+                    }
+
+                    if (_timeKeeper.TimedOut)
+                    {
+                        SaveDataForPoint();
+                    }
+                    else if (_data.Count < _sampleCount)
+                    {
+                        // We are only interested in valid samples. Here we consider both eyes.
+                        if (e.LeftEye.GazePoint.Validity == Validity.Valid && e.RightEye.GazePoint.Validity == Validity.Valid)
+                        {
+                            _data.Enqueue(e);
+                        }
+
+                        // We have reached our count. SaveDataForPoint changes state.
+                        if (_data.Count >= _sampleCount)
+                        {
+                            SaveDataForPoint();
+                        }
+                    }
+
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        private void SaveDataForPoint()
+        {
+            lock (_lock)
+            {
+                _dataMap.Add(new KeyValuePair<NormalizedPoint2D, Queue<GazeDataEventArgs>>(_currentPoint, _data));
+            }
+
+            _data = null;
+            State = ValidationState.NotCollecting;
+        }
     }
 }
---
+++ source/Tobii.Research.Addons/Utility/Utility.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Tobii.Research.Addons.Utility
+{
+    internal static class Extensions
+    {
+        internal static double Magnitude(float x, float y, float z)
+        {
+            return Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2) + Math.Pow(z, 2));
+        }
+
+        internal static double Magnitude(this Point3D vec)
+        {
+            return Magnitude(vec.X, vec.Y, vec.Z);
+        }
+
+        internal static double Magnitude(float x, float y)
+        {
+            return Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+        }
+
+        internal static double Magnitude(this NormalizedPoint2D vec)
+        {
+            return Magnitude(vec.X, vec.Y);
+        }
+
+        internal static Point3D Add(this Point3D me, Point3D other)
+        {
+            return new Point3D(me.X + other.X, me.Y + other.Y, me.Z + other.Z);
+        }
+
+        internal static Point3D Sub(this Point3D me, Point3D other)
+        {
+            return new Point3D(me.X - other.X, me.Y - other.Y, me.Z - other.Z);
+        }
+
+        internal static Point3D Mul(this Point3D me, double other)
+        {
+            return new Point3D(me.X * (float)other, me.Y * (float)other, me.Z * (float)other);
+        }
+
+        internal static Point3D Div(this Point3D me, double other)
+        {
+            return new Point3D(me.X / (float)other, me.Y / (float)other, me.Z / (float)other);
+        }
+
+        internal static Point3D Normalize(this Point3D vec)
+        {
+            var mag = (float)Magnitude(vec.X, vec.Y, vec.Z);
+            return new Point3D(vec.X / mag, vec.Y / mag, vec.Z / mag);
+        }
+
+        internal static Point3D NormalizedDirection(this Point3D start, Point3D end)
+        {
+            return Normalize(Direction(start, end));
+        }
+
+        internal static Point3D Direction(this Point3D start, Point3D end)
+        {
+            var x = end.X - start.X;
+            var y = end.Y - start.Y;
+            var z = end.Z - start.Z;
+            return new Point3D(x, y, z);
+        }
+
+        internal static double DotProduct(this Point3D me, Point3D other)
+        {
+            return me.X * other.X + me.Y * other.Y + me.Z * other.Z;
+        }
+
+        private static double Clamp(this double me, double low = -1d, double high = 1d)
+        {
+            me = Math.Min(high, me);
+            me = Math.Max(low, me);
+            return me;
+        }
+
+        internal static double Angle(this Point3D me, Point3D other)
+        {
+            return ((Math.Acos((me.DotProduct(other) / (me.Magnitude() * other.Magnitude())).Clamp()) * 180 / Math.PI) + 360) % 360;
+        }
+
+        public static double MeanAngle(this double[] angles)
+        {
+            var x = angles.Sum(a => Math.Cos(a * Math.PI / 180)) / angles.Length;
+            var y = angles.Sum(a => Math.Sin(a * Math.PI / 180)) / angles.Length;
+            return Math.Atan2(y, x) * 180.0 / Math.PI;
+        }
+
+        internal static Point3D Average(this Queue<GazeDataEventArgs> queue, Func<GazeDataEventArgs, Point3D> selector)
+        {
+            var x = 0.0;
+            var y = 0.0;
+            var z = 0.0;
+
+            foreach (var item in queue)
+            {
+                var p = selector(item);
+                x += p.X;
+                y += p.Y;
+                z += p.Z;
+            }
+
+            return new Point3D((float)(x / queue.Count), (float)(y / queue.Count), (float)(z / queue.Count));
+        }
+
+        internal static Point3D NormalizedPoint2DToPoint3D(this NormalizedPoint2D point2D, DisplayArea displayArea)
+        {
+            var dx = displayArea.TopRight.Sub(displayArea.TopLeft).Mul(point2D.X);
+            var dy = displayArea.BottomLeft.Sub(displayArea.TopLeft).Mul(point2D.Y);
+            return displayArea.TopLeft.Add(dx.Add(dy));
+        }
+    }
+
+    internal class TimeKeeper
+    {
+        private readonly object _lock = new object();
+        private Stopwatch _stopwatch = new Stopwatch();
+        private int _timeoutMS;
+
+        public bool TimedOut
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    return _stopwatch.ElapsedMilliseconds >= _timeoutMS;
+                }
+            }
+        }
+
+        public void Restart()
+        {
+            lock (_lock)
+            {
+                _stopwatch.Reset();
+                _stopwatch.Start();
+            }
+        }
+
+        public void Stop()
+        {
+            lock (_lock)
+            {
+                _stopwatch.Stop();
+            }
+        }
+
+        public TimeKeeper(int timeoutMS)
+        {
+            _timeoutMS = timeoutMS;
+        }
+    }
+}
